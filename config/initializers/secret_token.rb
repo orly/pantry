e3eb15dd49692e49463be60a8ddbb27d6d0b57a7
ryan <itsryanlol@gmail.com>
2010-08-10 20:44:38 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pantry::Application.config.secret_token = 'c8f7b62f03de2fd50424bcce40bd289591ad33652ddbc7b10deefe637f16b6cb916123160f20999442e82b1ec4d8be888d225cabc943595eaf006ddc44fda297'
